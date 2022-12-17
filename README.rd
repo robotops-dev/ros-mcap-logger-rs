f1e879252d79fb269a43a5fa391a7439f076142c
Joshua Chaitin-Pollak <josh@picklerobot.com>
2022-12-17 15:46:46 +0000

    initial commit - everything compiles, mcap written

---
+++ README.rd
@@ -0,0 +1,17 @@
+## Getting Started
+
+The easiest way to get started is to use the Devcontainer with VS Code, or GitHub Codespaces
+
+Once in the container, open a terminal and run:
+
+```
+. /opt/ros/noetic/setup.sh
+```
+
+Then you can run typical rust commands like:
+
+```
+cargo run
+```
+
+Good Hunting.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::{collections::BTreeMap, fs, io::BufWriter};
+
+use anyhow::Result;
+
+use mcap::{Channel, records::MessageHeader, Writer};
+
+fn write_it() -> Result<()> {
+    // To set the profile or compression options, see mcap::WriteOptions.
+    let mut out = Writer::new(
+        BufWriter::new(fs::File::create("out.mcap")?)
+    )?;
+
+    // Channels and schemas are automatically assigned ID as they're serialized,
+    // and automatically deduplicated with `Arc` when deserialized.
+    let my_channel = Channel {
+        topic: String::from("cool stuff"),
+        schema: None,
+        message_encoding: String::from("application/octet-stream"),
+        metadata: BTreeMap::default()
+    };
+
+    let channel_id = out.add_channel(&my_channel)?;
+
+    out.write_to_known_channel(
+        &MessageHeader {
+            channel_id,
+            sequence: 25,
+            log_time: 6,
+            publish_time: 24
+        },
+        &[1, 2, 3]
+    )?;
+    out.write_to_known_channel(
+        &MessageHeader {
+            channel_id,
+            sequence: 32,
+            log_time: 23,
+            publish_time: 25
+        },
+        &[3, 4, 5]
+    )?;
+
+    out.finish()?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    write_it();
 }
